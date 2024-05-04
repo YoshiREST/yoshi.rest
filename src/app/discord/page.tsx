13fb0284ceec6dda6bf0716f8cf37fe9f6a942ad
hxruhime <38597755+hxruhime@users.noreply.github.com>
2024-05-03 21:36:17 -0700

    add discord redirect

---
+++ src/app/discord/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React         from "react";
+
+const Discord = () => {
+
+    return (
+        <div>
+            <meta http-equiv="Refresh" content="0; url='https://discord.gg/xygu9NgPS7'" />
+        </div>
+    );
+};
+
+export default Discord;
